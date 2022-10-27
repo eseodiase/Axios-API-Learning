b6f9bc7779a77a91fc7e789d2ac1ee57c819b07c
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-27 13:01:41 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import {useState} from 'react';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+function App(){
+    const [quote, setQuote] = useState('')
+    const getQuote = () =>{
+        axios.get('https://api.quotable.io/radom').then (res =>{
+            console.log(res.data.content)
+            setQuote(res.data.content)
+        }).catch (err =>{
+            console.log(err)
+        })
+    }
+    
+    return(
+    <div className="App">
+    <button onClick={getQuote}> Get Quote </button>
+    (quote && <p>{quote}</p> )
     </div>
-  );
+    )
 }
+
+export default App;
