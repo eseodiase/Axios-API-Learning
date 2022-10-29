4a1f1582b8837f33db59133c9e0eb648f625fe59
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-30 00:54:19 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
