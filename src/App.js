447fcbf56eb00d46f5d67e7f3fe26d811b973723
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-29 16:51:26 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App(){
+    const [author, setAuthor] = useState('')
     const [quote, setQuote] = useState('')
     const getQuote = () =>{
         axios.get('https://api.quotable.io/random').then (res =>{
             console.log(res.data.content)
+            setAuthor(res.data.author)
             setQuote(res.data.content)
         }).catch (err =>{
             console.log(err)
@@ -16,7 +18,8 @@ function App(){
     return(
     <div className="App">
     <button onClick={getQuote}> Get Quote </button>
-    (quote && <p>{quote}</p> )
+    <p>{author}</p>
+    <p>{quote}</p>
     </div>
     )
 }
