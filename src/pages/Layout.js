5fbee84055c4d4ba28016721e79207a440d33b05
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-30 00:49:47 +0100

    Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,1 +1,25 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
+const Layout = () => {
+  return (
+      <>
+      <nav>
+      <ul>
+         <li className="logo">
+<i className="fa fa-home"></i>
+
+<Link style={{textDecoration: 'none', color: '#ffffff'}} to="/">Home</Link>
+         </li>
+         <li className="about">
+<Link style={{textDecoration: 'none', color: '#ffffff'}} to="/about">About</Link>
+         </li>                                                                                            
+                                                                                   
+         </ul>
+         </nav>
+
+         <Outlet />
+         </>
+         )
+         };
+export default Layout;
